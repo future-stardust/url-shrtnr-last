520c338dabd8836e1a7dcdd8b8b3f5be8deacfc9
TymurL <logvinenko814@gmail.com>
2021-03-04 22:44:07 +0200

    Add implementation that saves users to file

--- src/main/java/edu/kpi/testcourse/model/User.java
+++ src/main/java/edu/kpi/testcourse/model/User.java
@@ -1,9 +1,38 @@
 package edu.kpi.testcourse.model;
 
-import java.io.Serializable;
-
 /**
  * User model which contains info about user.
  */
-public record User(String email, String password) implements Serializable {
+public class User {
+  private String email;
+  private String password;
+
+  public User(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "User{" +
+      "email='" + email + '\'' +
+      ", password='" + password + '\'' +
+      '}';
+  }
 }
--- src/main/java/edu/kpi/testcourse/repository/UserRepositoryImpl.java
+++ src/main/java/edu/kpi/testcourse/repository/UserRepositoryImpl.java
@@ -1,6 +1,13 @@
 package edu.kpi.testcourse.repository;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import edu.kpi.testcourse.model.User;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
@@ -9,18 +16,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Simple implementation of user storage that uses java serialization to save data in file.
+ * Simple implementation of user storage that uses gson to save data in file.
  */
 @Singleton
 public class UserRepositoryImpl implements UserRepository {
 
   private static final Logger logger = LoggerFactory.getLogger(UserRepositoryImpl.class);
-  private final Map<String, User> users = new ConcurrentHashMap<>();
+  private final Map<String, User> users;
+  private final Gson gson;
+  private final String fileName = "users.txt";
+
+  public UserRepositoryImpl() {
+    this.gson = new Gson();
+    this.users = initFileStorage();
+  }
 
   @Override
   public void save(User user) {
     logger.debug("Saving user={}", user.toString());
-    users.put(user.email(), user);
+    users.put(user.getEmail(), user);
+    writeUsersToFile();
   }
 
   @Override
@@ -31,5 +46,41 @@ public class UserRepositoryImpl implements UserRepository {
   @Override
   public Optional<User> getUserByEmail(String email) {
     return Optional.ofNullable(users.get(email));
+  }
+
+  private void writeUsersToFile() {
+    try (FileWriter fileWriter = new FileWriter(fileName)) {
+      fileWriter.write(gson.toJson(users));
+    } catch (IOException e) {
+      logger.error("Can not write users to file");
+      throw new Error("Error while writing users to file");
+    }
+  }
+
+  private Map<String, User> readUsersFromFile() {
+    File file = new File(fileName);
+    Type type = new TypeToken<Map<String, User>>(){}.getType();
+    try {
+      String json = Files.readString(file.toPath());
+      return gson.fromJson(json, type);
+    } catch (IOException e) {
+      logger.error("Can not read users from file");
+      throw new Error("Error while reading users from file");
+    }
+  }
+
+  private Map<String, User> initFileStorage() {
+    File file = new File(fileName);
+    try {
+      if (file.createNewFile()) {
+        logger.info("File for user storage has been created");
+        return new ConcurrentHashMap<>();
+      } else {
+        return new ConcurrentHashMap<>(readUsersFromFile());
+      }
+    } catch (IOException e) {
+      logger.error("Can not create file for user storage", e);
+      throw new Error("Error while creating file for user storage");
+    }
   }
 }
