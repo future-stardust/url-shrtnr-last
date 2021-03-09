d959115d5d5b5b857e1024ee4914e24e0a6cc4c7
TymurL <logvinenko814@gmail.com>
2021-03-09 14:56:05 +0200

    Modify session repository to store active sessions in file

--- src/test/java/edu/kpi/testcourse/repository/ActiveSessionRepositoryTest.java
+++ src/test/java/edu/kpi/testcourse/repository/ActiveSessionRepositoryTest.java
@@ -3,15 +3,23 @@ package edu.kpi.testcourse.repository;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import java.io.File;
 import javax.inject.Inject;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 @MicronautTest
 class ActiveSessionRepositoryTest {
 
   @Inject
   ActiveSessionRepository sessionRepository;
+  @Property(name = "db.sessions.filename")
+  String fileName;
 
   @Test
   void testSaveJwt() {
@@ -19,5 +27,11 @@ class ActiveSessionRepositoryTest {
     assertTrue(sessionRepository.contains("abc"));
     sessionRepository.remove("abc");
     assertFalse(sessionRepository.contains("abc"));
+  }
+
+  @AfterAll
+  void cleanDatabase() {
+    File file = new File(fileName);
+    if (!file.delete()) throw new Error("Can not delete file after testing");
   }
 }
