acb712db00d74ebdd71d37b6a387aca0a79028e2
TymurL <logvinenko814@gmail.com>
2021-03-14 12:22:26 +0200

    Get back authentication and authorization from micronaut

--- src/test/java/edu/kpi/testcourse/controller/UserControllerTest.java
+++ src/test/java/edu/kpi/testcourse/controller/UserControllerTest.java
@@ -2,15 +2,12 @@ package edu.kpi.testcourse.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import edu.kpi.testcourse.model.User;
-import edu.kpi.testcourse.repository.ActiveSessionRepository;
-import edu.kpi.testcourse.repository.ActiveSessionRepositoryImpl;
 import edu.kpi.testcourse.repository.UserRepository;
 import edu.kpi.testcourse.repository.UserRepositoryImpl;
 import io.micronaut.http.HttpRequest;
@@ -23,7 +20,6 @@ import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import java.util.Optional;
 import javax.inject.Inject;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
@@ -37,41 +33,9 @@ class UserControllerTest {
   @Inject
   UserRepository userRepository;
 
-  @Inject
-  ActiveSessionRepository activeSessionRepository;
-
   @MockBean(UserRepositoryImpl.class)
   UserRepository userRepo() {
     return mock(UserRepository.class);
-  }
-
-  @MockBean(ActiveSessionRepositoryImpl.class)
-  ActiveSessionRepository sessionRepo() {
-    return mock(ActiveSessionRepository.class);
-  }
-
-  @Test
-  void testUserLoginSuccessful() {
-    when(userRepository.getUserByEmail("user1@mail.com")).thenReturn(Optional.of(new User("user1@mail.com", "pass123")));
-
-    HttpResponse<String> response = client.toBlocking()
-      .exchange(HttpRequest.POST("/users/signin", new User("user1@mail.com", "pass123")), String.class);
-
-    assertEquals(response.status(), HttpStatus.OK);
-    Assertions.assertThat(response.body()).matches("\\{\"access_token\":\"[A-Za-z0-9_-]+\\.[A-Za-z0-9_-]+\\.[A-Za-z0-9_-]+\"}");
-    verify(activeSessionRepository).save(anyString());
-  }
-
-  @Test
-  void testUserLoginFailed() {
-    when(userRepository.getUserByEmail("user1@mail.com")).thenReturn(Optional.of(new User("user1@mail.com", "pass123")));
-
-    Executable e = () -> client.toBlocking()
-      .exchange(HttpRequest.POST("/users/signin", new User("us@mail.com", "pass123")), String.class);
-
-    HttpClientResponseException thrown = assertThrows(HttpClientResponseException.class, e);
-    assertEquals(thrown.getStatus(), HttpStatus.UNAUTHORIZED);
-    verify(activeSessionRepository, never()).save(anyString());
   }
 
   @Test
