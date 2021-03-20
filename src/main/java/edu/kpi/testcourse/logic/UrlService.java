71dd80c1df5c46c515d0be7f832850ebcbc7050b
TymurL <logvinenko814@gmail.com>
2021-03-20 14:46:55 +0200

    Add alias generation

--- src/main/java/edu/kpi/testcourse/logic/UrlService.java
+++ src/main/java/edu/kpi/testcourse/logic/UrlService.java
@@ -3,6 +3,7 @@ package edu.kpi.testcourse.logic;
 import edu.kpi.testcourse.model.UrlAlias;
 import edu.kpi.testcourse.repository.UrlRepository;
 import java.util.List;
+import java.util.Random;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -35,13 +36,17 @@ public class UrlService {
   public String createNewAlias(String email, String url, String alias) throws Exception {
     String finalAlias;
     if (alias == null || alias.isEmpty()) {
-      // TODO: Generate short alias
-      throw new UnsupportedOperationException("Is not implemented yet");
+      finalAlias = generateAlias();
     } else {
       finalAlias = alias;
     }
 
-    urlRepository.save(new UrlAlias(finalAlias, url, email));
+    try {
+      urlRepository.save(new UrlAlias(finalAlias, url, email));
+    } catch (Exception e) {
+      finalAlias = generateAlias();
+      urlRepository.save(new UrlAlias(finalAlias, url, email));
+    }
 
     return finalAlias;
   }
@@ -63,5 +68,17 @@ public class UrlService {
       }
     }
     return false;
+  }
+
+  private String generateAlias() {
+    int leftLimit = 97; // letter 'a'
+    int rightLimit = 122; // letter 'z'
+    int stringLength = 5;
+    Random random = new Random();
+
+    return random.ints(leftLimit, rightLimit + 1)
+      .limit(stringLength)
+      .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+      .toString();
   }
 }
